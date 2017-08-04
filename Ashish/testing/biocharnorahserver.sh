20b0db3d6291f8a6010d932cad325e78c4757f29
Ashish Gupta <ashishlnmiit3@gmail.com>
2017-08-04 15:04:01 +0530

    shell script+render json fix

--- Ashish/testing/biocharnorahserver.sh
+++ Ashish/testing/biocharnorahserver.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python2 ~/makehuman/makehuman/makehuman.py --headless --runserver $@ >/dev/null 2>&1
+pidof Xorg || sudo Xorg &
+sudo DISPLAY=:0 python2 ~/makehuman/makehuman/makehuman.py --headless --runserver $@ >/dev/null 2>&1
 echo Done
